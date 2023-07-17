4f741a60d3179b55812acd1730b126d6a193b6f2
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-07-17 11:02:59 -0300

    Formação Spring-Framework - TreinaWeb - Consumindo Api-CEP WebMania

---
+++ src/main/java/br/com/treinaweb/cepautocomplete/dto/WebManiaResponse.java
@@ -0,0 +1,20 @@
+package br.com.treinaweb.cepautocomplete.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WebManiaResponse {
+
+    private String endereco;
+    private String bairro;
+
+    private String cidade;
+    private String uf;
+    private String cep;
+    private String ibge;
+
+}
---
+++ src/main/java/br/com/treinaweb/cepautocomplete/servicos/CepServico.java
@@ -0,0 +1,19 @@
+package br.com.treinaweb.cepautocomplete.servicos;
+
+import br.com.treinaweb.cepautocomplete.dto.WebManiaResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class CepServico {
+    private static final String URL_TEMPLATE = "https://webmaniabr.com/api/1/cep/%s/?app_key=QpgzByU0G8bRalamUzgxmwk92BUc9sa3&app_secret=yoXv0oOpAEgFT0qDL4waWTA4P0lrAIWBXktLoZrC4AK6g69m";
+
+    public WebManiaResponse consultar (String cep){
+        String url = String.format(URL_TEMPLATE, cep);
+        RestTemplate restTemplate = new RestTemplate();
+        WebManiaResponse response = restTemplate.getForObject(url, WebManiaResponse.class);
+        return response;
+    }
+
+
+}
